ff8b2222f61662ebd87da5662441c70fba167b2b
Rodrigo Rodriguez <rodrigo.rodriguez@marandu.com.ar>
2021-01-16 20:50:12 -0300

    adding something like routes

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types';
 
 export const Navbar = () => {
+
+    const { user, dispatch } = useContext(AuthContext);
+
+    const history = useHistory();
+
+    const handleLogout = () => {
+        dispatch({
+            type: types.logout,
+            payload: {
+            }
+        });
+        history.replace('/login');
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
+
+            <Link
+                className="navbar-brand"
                 to="/"
             >
                 Asociaciones
@@ -15,27 +31,27 @@ export const Navbar = () => {
             <div className="navbar-collapse">
                 <div className="navbar-nav">
 
-                    <NavLink 
+                    <NavLink
                         activeClassName="active"
-                        className="nav-item nav-link" 
+                        className="nav-item nav-link"
                         exact
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
-                    <NavLink 
+                    <NavLink
                         activeClassName="active"
-                        className="nav-item nav-link" 
+                        className="nav-item nav-link"
                         exact
                         to="/dc"
                     >
                         DC
                     </NavLink>
 
-                    <NavLink 
+                    <NavLink
                         activeClassName="active"
-                        className="nav-item nav-link" 
+                        className="nav-item nav-link"
                         exact
                         to="/search"
                     >
@@ -46,14 +62,10 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
-                    >
+                    <span className="nav-item nav-link text-info" style={{paddingRight: 50}}> {user.name} </span>
+                    <button className="nav-item nav-link btn btn-outline-primary" onClick={handleLogout}>
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
