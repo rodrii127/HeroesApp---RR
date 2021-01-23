cabb8dd8e30cfd5617011eb4c270dc4b12a2b527
Rodrigo Rodriguez <rodrigo.rodriguez@marandu.com.ar>
2021-01-23 12:30:27 -0300

    a

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -9,7 +9,7 @@ export const HeroCard = ( hero ) => {
         <div className='card ms-3' style= { {maxWidth: 540} }>
             <div className='row no-gutters'>
                 <div className='col-md-4'>
-                    <img src={`./HeroesApp---RR/assets/heroes/${ id }.jpg`} className='card-img' alt={ superhero }/>
+                    <img src={`./HeroesApp---RR/falopa/heroes/${ id }.jpg`} className='card-img' alt={ superhero }/>
                 </div>
                 <div className='col-md-8'>
                     <div className='card-body'>
